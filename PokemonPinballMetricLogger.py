f40b25cf808598c1313e7cbec6efa50d6601d942
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2024-03-12 14:00:45 -0700

    new logger with episode tracking

--- PokemonPinballMetricLogger.py
+++ PokemonPinballMetricLogger.py
@@ -1,78 +1,52 @@
 import numpy as np
 import time, datetime
 import matplotlib.pyplot as plt
-
+from pathlib import Path
 
 class MetricLogger:
-    def __init__(self, save_dir):
-        self.save_log = save_dir / "log"
-        if self.save_log.exists():
-            print("WARNING: CONITNUING FROM PREVIOUS TRAINING SESSION DOES NOT LOG PROPERLY!!!!")
-            with open(self.save_log, "r") as f:
-                lines = f.readlines()[1:]  # Skip the header line
-                self.ep_rewards = [float(line.split()[3]) for line in lines]
-                self.ep_lengths = [int(line.split()[1]) for line in lines]
-                self.ep_avg_losses = [float(line.split()[4]) for line in lines]
-                self.ep_avg_qs = [float(line.split()[5]) for line in lines]
-          # Calculate moving averages from loaded data
-            self.moving_avg_ep_rewards = self.calculate_moving_average(self.ep_rewards)
-            self.moving_avg_ep_lengths = self.calculate_moving_average(self.ep_lengths)
-            self.moving_avg_ep_avg_losses = self.calculate_moving_average(self.ep_avg_losses)
-            self.moving_avg_ep_avg_qs = self.calculate_moving_average(self.ep_avg_qs)
-    
-        else:
-            with open(self.save_log, "w") as f:
-                f.write(
-                    f"{'Episode':>8}{'Step':>8}{'Epsilon':>10}{'MeanReward':>15}"
-                    f"{'MeanLength':>15}{'MeanLoss':>15}{'MeanQValue':>15}"
-                    f"{'TimeDelta':>15}{'Time':>20}\n"
-                )
-            self.ep_rewards = []
-            self.ep_lengths = []
-            self.ep_avg_losses = []
-            self.ep_avg_qs = []
+    def __init__(self, save_dir, resume=False):
+        self.resume = resume
+        self.save_log = save_dir / "log.txt"  # Ensure the file extension is specified
+        self.save_dir = save_dir
+        self.load_log()
 
         self.ep_rewards_plot = save_dir / "reward_plot.jpg"
         self.ep_lengths_plot = save_dir / "length_plot.jpg"
         self.ep_avg_losses_plot = save_dir / "loss_plot.jpg"
         self.ep_avg_qs_plot = save_dir / "q_plot.jpg"
 
-        # Moving averages, added for every call to record()
-        self.moving_avg_ep_rewards = []
-        self.moving_avg_ep_lengths = []
-        self.moving_avg_ep_avg_losses = []
-        self.moving_avg_ep_avg_qs = []
-
-        # Current episode metric
-        self.init_episode()
-
         # Timing
         self.record_time = time.time()
 
-    def calculate_moving_average(self, data, window_size=100):
-        return [np.mean(data[max(0, i-window_size+1):i+1]) for i in range(len(data))]
-    def log_step(self, reward, loss, q):
-        self.curr_ep_reward += reward
-        self.curr_ep_length += 1
-        if loss:
-            self.curr_ep_loss += loss
-            self.curr_ep_q += q
-            self.curr_ep_loss_length += 1
-
-    def log_episode(self):
-        "Mark end of episode"
-        self.ep_rewards.append(self.curr_ep_reward)
-        self.ep_lengths.append(self.curr_ep_length)
-        if self.curr_ep_loss_length == 0:
-            ep_avg_loss = 0
-            ep_avg_q = 0
+    def load_log(self):
+        if self.save_log.exists() and self.resume:
+            data = np.loadtxt(self.save_log, skiprows=1, unpack=True)
+            if data.size > 0:
+                self.episode, self.step, self.epsilon, self.ep_rewards, self.ep_lengths, self.ep_avg_losses, self.ep_avg_qs = data
+                self.episode = list(self.episode.astype(int))
+                self.step = list(self.step.astype(int))
+                self.ep_rewards = list(self.ep_rewards)
+                self.ep_lengths = list(self.ep_lengths)
+                self.ep_avg_losses = list(self.ep_avg_losses)
+                self.ep_avg_qs = list(self.ep_avg_qs)
+            else:  # Handle empty log file with header
+                self.reset_lists()
         else:
-            ep_avg_loss = np.round(self.curr_ep_loss / self.curr_ep_loss_length, 5)
-            ep_avg_q = np.round(self.curr_ep_q / self.curr_ep_loss_length, 5)
-        self.ep_avg_losses.append(ep_avg_loss)
-        self.ep_avg_qs.append(ep_avg_q)
-
+            self.reset_lists()
+            with open(self.save_log, "w") as f:
+                f.write(
+                    "Episode    Step    Epsilon    MeanReward    MeanLength    MeanLoss    MeanQValue\n"
+                )
         self.init_episode()
+
+    def reset_lists(self):
+        self.episode = []
+        self.step = []
+        self.epsilon = []
+        self.ep_rewards = []
+        self.ep_lengths = []
+        self.ep_avg_losses = []
+        self.ep_avg_qs = []
 
     def init_episode(self):
         self.curr_ep_reward = 0.0
@@ -81,42 +55,66 @@ class MetricLogger:
         self.curr_ep_q = 0.0
         self.curr_ep_loss_length = 0
 
+    def log_step(self, reward, loss, q):
+        self.curr_ep_reward += reward
+        self.curr_ep_length += 1
+        if loss is not None:
+            self.curr_ep_loss += loss
+            self.curr_ep_q += q
+            self.curr_ep_loss_length += 1
+
+    def log_episode(self):
+        if self.curr_ep_loss_length == 0:
+            ep_avg_loss = 0
+            ep_avg_q = 0
+        else:
+            ep_avg_loss = np.round(self.curr_ep_loss / self.curr_ep_loss_length, 5)
+            ep_avg_q = np.round(self.curr_ep_q / self.curr_ep_loss_length, 5)
+        self.ep_rewards.append(self.curr_ep_reward)
+        self.ep_lengths.append(self.curr_ep_length)
+        self.ep_avg_losses.append(ep_avg_loss)
+        self.ep_avg_qs.append(ep_avg_q)
+        self.init_episode()
+
     def record(self, episode, epsilon, step):
         mean_ep_reward = np.round(np.mean(self.ep_rewards[-100:]), 3)
         mean_ep_length = np.round(np.mean(self.ep_lengths[-100:]), 3)
         mean_ep_loss = np.round(np.mean(self.ep_avg_losses[-100:]), 3)
         mean_ep_q = np.round(np.mean(self.ep_avg_qs[-100:]), 3)
-        self.moving_avg_ep_rewards.append(mean_ep_reward)
-        self.moving_avg_ep_lengths.append(mean_ep_length)
-        self.moving_avg_ep_avg_losses.append(mean_ep_loss)
-        self.moving_avg_ep_avg_qs.append(mean_ep_q)
 
         last_record_time = self.record_time
         self.record_time = time.time()
-        time_since_last_record = np.round(self.record_time - last_record_time, 3)
+        time_delta = np.round(self.record_time - last_record_time, 3)
 
         print(
             f"Episode {episode} - "
             f"Step {step} - "
-            f"Epsilon {epsilon} - "
+            f"Epsilon {epsilon:.3f} - "
             f"Mean Reward {mean_ep_reward} - "
             f"Mean Length {mean_ep_length} - "
             f"Mean Loss {mean_ep_loss} - "
             f"Mean Q Value {mean_ep_q} - "
-            f"Time Delta {time_since_last_record} - "
-            f"Time {datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}"
+            f"Time Delta {time_delta} - "
+            f"Time {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
         )
 
         with open(self.save_log, "a") as f:
             f.write(
-                f"{episode:8d}{step:8d}{epsilon:10.3f}"
-                f"{mean_ep_reward:15.3f}{mean_ep_length:15.3f}{mean_ep_loss:15.3f}{mean_ep_q:15.3f}"
-                f"{time_since_last_record:15.3f}"
-                f"{datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'):>20}\n"
+                f"{episode:<10}{step:<10}{epsilon:<10.3f}"
+                f"{mean_ep_reward:<15}{mean_ep_length:<15}{mean_ep_loss:<15}{mean_ep_q:<15}\n"
             )
+        self.plot_metrics()
 
-        for metric in ["ep_lengths", "ep_avg_losses", "ep_avg_qs", "ep_rewards"]:
+    def plot_metrics(self):
+        metrics = [("ep_rewards", "Reward"), ("ep_lengths", "Length"), ("ep_avg_losses", "Loss"), ("ep_avg_qs", "Q Value")]
+        for metric, name in metrics:
             plt.clf()
-            plt.plot(getattr(self, f"moving_avg_{metric}"), label=f"moving_avg_{metric}")
+            plt.title(f"Moving Average of {name}")
+            plt.plot(self.calculate_moving_average(getattr(self, metric)), label=f"Moving Avg {name}")
+            plt.xlabel("Episode")
+            plt.ylabel(name)
             plt.legend()
-            plt.savefig(getattr(self, f"{metric}_plot"))
+            plt.savefig(self.save_dir / f"{metric}_plot.jpg")
+
+    def calculate_moving_average(self, data, window_size=100):
+        return [np.mean(data[max(0, i - window_size + 1):i + 1]) for i in range(len(data))]
